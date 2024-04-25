ac368670f356c4e5962e4e6a7618b5a40e85c660
Helien-Dev <alvaroaviladev@gmail.com>
2024-04-25 13:48:59 -0500

    Yt transcript with assamblyAI

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,14 +1,83 @@
+import os
+import json
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.conf import settings
+from pytube import YouTube
+import assemblyai as aai
 
 
 # Create your views here.
+
+
+#
+@login_required
 def index(request):
     return render(request, "index.html")
 
 
+@csrf_exempt
+def generate_blog(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            yt_link = data["link"]
+        except (KeyError, json.JSONDecodeError):
+            return JsonResponse({"error": "Invalid request sent"}, status=404)
+            
+        # get yt title
+        title = yt_title(yt_link)
+        # get transcript
+        transcription = get_transcription(yt_link)
+        if not transcription :
+            return JsonResponse({'error': 'Failed to get transcript'}, status=500)
+            # use openAI to generate the blog
+            # save blog article to databse
+            # return blog article as a response    
+    else:
+        return JsonResponse({"error": "Invalid request method"}, status=405)
+
+
+def yt_title(link):
+    yt = YouTube
+    title = yt.title
+    return title
+
+def download_audio(link):
+    yt = YouTube(link)
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download(output_path=settings.MEDIA_ROOT)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    return new_file
+
+def get_transcription(link):
+    audio_file = download_audio(link)
+    aai.settings.api_key = '56cd8ec5ba7d4d13ab4291ca08257b9f'
+    
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(audio_file)
+    return transcript.text
+    
+
 def user_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            error_message = "Invalid username or password"
+            return render(request, "login.html", {"error_message": error_message})
+
     return render(request, "login.html")
 
 
@@ -36,4 +105,5 @@ def user_signup(request):
 
 
 def user_logout(request):
-    return render(request, "logout.html")
+    logout(request)
+    return redirect("/")
